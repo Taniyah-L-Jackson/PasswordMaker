749bca7072388f022ac293fe7c88e6d272c0a952
Taniyah-L-Jackson <taniyah-l-jackson-h@gmail.com>
2020-06-30 22:12:23 -0500

    updated ending scene based on location. Updated verify button.

--- js/app.js
+++ js/app.js
@@ -1,18 +1,19 @@
-//password must follow rules (in slack)
-//password inputted a second time must match the password inputted first
-//have regex if-else statements
-//enter verification password fade in after password is entered
-//tester only has five times to input password
-//after five, close site
-//-----------------------------------------------------------------------------
+//for glitch
+var box = document.getElementById("box")
+var failed = document.getElementById("failed")
 
-document.getElementById("proceed").style.visibility = "hidden"; //hides the proceed button until its called on in the code
+//--handle visibility here for code to work
+box.style.display = 'block'
+failed.style.display = 'none'
+
+//hides the proceed button until its called on in the code
+document.getElementById("proceed").style.visibility = "hidden" 
 
 function access() {
 
     //variable list
     let name = document.getElementById('user').value //get user to input a username
-    let pass = document.getElementById('pass').value; //get user to set a password
+    let pass = document.getElementById('pass').value //get user to set a password
 
     let passRegexUpper = /[A-Z]/g //check for capital letters
     let passRegexLower = /[a-z]/g //check for lowercase letters
@@ -46,38 +47,38 @@ function access() {
 
                     valid.innerText = "Password Accepted. Proceed." 
                       
-                    document.getElementById("proceed").style.visibility = "visible";
+                    document.getElementById("proceed").style.visibility = "visible"
                     //shows button that triggers an event
 
                 }else{ //condition is not met
                     valid.innerText = "Password is invalid. No numbers or Special characters included "
 
-                    document.getElementById("proceed").style.visibility = "hidden"; 
-                    document.getElementById("verifyPass").classList.remove("fadeIn");//re-hides button  and button function in case the user clears the code the first time and tries to break it the next time
+                    document.getElementById("proceed").style.visibility = "hidden" 
+                    document.getElementById("verifyPass").classList.remove("fadeIn")//re-hides button  and button function in case the user clears the code the first time and tries to break it the next time
                 }
 
             }else{ //condition is not met
                 valid.innerText = "Password is invalid. Missing lowercase/Uppercase letter"
-                document.getElementById("proceed").style.visibility = "hidden";
-                document.getElementById("verifyPass").classList.remove("fadeIn");
+                document.getElementById("proceed").style.visibility = "hidden"
+                document.getElementById("verifyPass").classList.remove("fadeIn")
             }
 
         }else { //condition is not met
             valid.innerText = "Password is invalid. Spacing included"
-            document.getElementById("proceed").style.visibility = "hidden";
-            document.getElementById("verifyPass").classList.remove("fadeIn");
+            document.getElementById("proceed").style.visibility = "hidden"
+            document.getElementById("verifyPass").classList.remove("fadeIn")
         }
 
     }else if(pass.length < 8 && pass.length != 0) {
         valid.innerText = "Password is too short"
-        document.getElementById("verifyPass").classList.remove("fadeIn");
+        document.getElementById("verifyPass").classList.remove("fadeIn")
     }
 
 }
 
 function verification() { //adds a class to the verifying part of the HTML (the button above)
     
-    document.getElementById("verifyPass").classList.add("fadeIn"); //fades the verifying part in using css
+    document.getElementById("verifyPass").classList.add("fadeIn") //fades the verifying part in using css
 }
 
 var counter = 5
@@ -92,43 +93,49 @@ function verify () {
     //---------------------------------------------------------------------------------
     //Code: checks for match
 
-    if(pass.value == match.value) { //is it necessary to use strict operator? possibly
+    if(pass.value === match.value) { // use strict
 
         entry.innerText = "Access Granted."
 
     }else { //if the pass does not match
          
         counter-- //decrement the counter by one
+
         entry.innerText = ("Incorrect. Tries left: " + counter ) //shows in HTML how many tries are left
         
-        if (counter == 0) { //once this happens, shut down site
+        if (counter <= 0) { //once this happens, shut down site
+
+            //get button id and disable it
+            document.getElementById('verify').disabled = 'true'
             
             let sec = 5 //stating point that shows a coundown in HTML
 
-            setInterval(function() { //Pauses long enough for user to read line and setInterval shows a functioning timer
+            let timer = setInterval(function() { //Pauses long enough for user to read line and setInterval shows a functioning timer
 
                 sec--
-                verification.innerHTML = "Failed too many times. BEGIN FORCE TERMINATION: " + sec; //shows when input is incorrect too many times
+                verification.innerHTML = "Failed too many times. BEGIN FORCE TERMINATION: " + sec //shows when input is incorrect too many times
 
-                verification.style.color = 'red'; //changes the style's original color to chosen color
+                verification.style.color = 'red' //changes the style's original color to chosen color
                 
                 if (sec <= 0) {
+
+                    // User is denied
+                    box.style.display = 'none'
+                    failed.style.display = 'block'
+
+                    //stop timer from reaching negative values
+                    clearInterval(timer) 
+
+                    // Special Feature: Use Only if Code is Downloaded
+                    // window.close() 
                     //closes whenever the seconds are at or below 0
-                    window.close(); //close site in 5 seconds (1000ms = 1s) 
+                    //close site in 5 seconds (1000ms = 1s) 
                 }
             
-            }, 1000); // time inbetween each second
+            }, 1000) // time inbetween each second
 
         }
-
+        
     }
 }
 
-//use .test() to use an if else statement with a boolean (for regex)
-//use style.display to use css 
-//else statements arent always needed
-//you can use break to interrupt the if else statement to go to the next one rather than going though each if else statement
-//objects are preferred over arrays because instead of calling the index, you can call the key value that stores the info you would like to use, and key values have unique names
-//For loops: runs a specific number of times, but can quit whenever it wants to
-//While loop: waits for a specific behavior or condition
-// For Each loop: Needs to process each piece of data and cant break 
